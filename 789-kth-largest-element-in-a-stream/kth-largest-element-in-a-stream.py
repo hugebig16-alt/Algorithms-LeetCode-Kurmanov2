2be0372796eb9f79030bfc83caf0eadd254a087b
hugebig16-alt <hugebig16@gmail.com>
2026-02-24 21:14:06 +0500

    Time: 53 ms (72.76%) | Memory: 17.9 MB (95.43%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -0,0 +1,18 @@
+import heapq
+
+class KthLargest:
+    def __init__(self, k, nums):  # без type hints
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val):
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        elif val > self.heap[0]:
+            heapq.heappop(self.heap)
+            heapq.heappush(self.heap, val)
+        return self.heap[0]
+        
