f35629114ba7ed7130c00140d7d5208c812b280e
hugebig16-alt <hugebig16@gmail.com>
2026-02-24 23:50:51 +0500

    Time: 62 ms (50.78%) | Memory: 17.9 MB (80.45%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,18 +1,23 @@
 import heapq
 
 class KthLargest:
-    def __init__(self, k, nums):  # без type hints
+    def __init__(self, k, nums):
         self.k = k
         self.heap = nums
         heapq.heapify(self.heap)
+
+        # Оставляем в куче только k самых больших элементов
         while len(self.heap) > k:
             heapq.heappop(self.heap)
 
     def add(self, val):
+        # Если элементов меньше k, просто добавляем
         if len(self.heap) < self.k:
             heapq.heappush(self.heap, val)
+        # Если новый элемент больше минимального в куче
         elif val > self.heap[0]:
             heapq.heappop(self.heap)
             heapq.heappush(self.heap, val)
+
+        # Минимальный элемент — это k-й наибольший
         return self.heap[0]
-        
