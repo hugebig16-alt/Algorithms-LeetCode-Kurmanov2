1af728765a6f6ef8045303e19f3637be85b1248d
hugebig16-alt <hugebig16@gmail.com>
2026-02-24 23:55:38 +0500

    Time: 13 ms (43.81%) | Memory: 14.2 MB (90.60%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,6 +3,10 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums, k):
-        count = Counter(nums)  # считаем частоты
-        # используем heap, чтобы получить k самых частых элементов
-        return [item for item, freq in heapq.nlargest(k, count.items(), key=lambda x: x[1])]
+        # Считаем, сколько раз встречается каждый элемент
+        count = Counter(nums)
+
+        # Берем k элементов с наибольшей частотой
+        return [item for item, freq in heapq.nlargest(
+            k, count.items(), key=lambda x: x[1]
+        )]
